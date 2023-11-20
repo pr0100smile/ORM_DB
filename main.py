6c5fa4d837d6a0d81e29ac8ea81a46afd777cca1
Dmitriy NetoIT <pr0100_smile@mail.ru>
2023-11-20 08:19:53 +0300

    error's correction (teacher's comment)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sqlalchemy as sq
 from sqlalchemy.orm import sessionmaker
 import datetime
-from models import create_tables, Publisher, Book, Shop, Stock, Sale
+from models import create_tables, get_shops, Publisher, Book, Shop, Stock, Sale
+
 
 SQLsys = 'postgresql'
 login = 'LOGIN'
@@ -10,28 +11,31 @@ host = 'localhost'
 port = 5432
 name_db = 'NAME DB'
 
-DSN = f'{SQLsys}://{login}:{password}@{host}:{port}/{name_db}'
+DSN = f'{SQLsys}+psycopg2://{login}:{password}@{host}:{port}/{name_db}'
 engine = sq.create_engine(DSN)
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 create_tables(engine)
 
-pub1 = Publisher(name='Пушкин', books = [
-        Book(title="Капитанская дочка"),
-        Book(title="Зимнее утро"),
-        Book(title="Полтава"),
-        Book(title="Сказка о царе Салтане")])
+pub1 = Publisher(name='Пушкин', books=[
+    Book(title="Капитанская дочка"),
+    Book(title="Зимнее утро"),
+    Book(title="Полтава"),
+    Book(title="Сказка о царе Салтане")])
 
-pub2 = Publisher(name='Лермонтов', books = [
-        Book(title="Герой нашего времени"),
-        Book(title="Мцыри"),
-        Book(title="Маскарад"),
-        Book(title="Пророк")])
+pub2 = Publisher(name='Лермонтов', books=[
+    Book(title="Герой нашего времени"),
+    Book(title="Мцыри"),
+    Book(title="Маскарад"),
+    Book(title="Пророк")])
 
-pub3 = Publisher(name='Толстой', books = [
-        Book(title="Война и мир"),
-        Book(title="Анна Каренина"),
-        Book(title="Воскресение"),
-        Book(title="После бала")])
+pub3 = Publisher(name='Толстой', books=[
+    Book(title="Война и мир"),
+    Book(title="Анна Каренина"),
+    Book(title="Воскресение"),
+    Book(title="После бала")])
 
 book1 = Book(title="Руслан и Людмила", publisher=pub1)
 book2 = Book(title="Смерть поэта", publisher=pub2)
@@ -39,29 +43,21 @@ book3 = Book(title="Детство", publisher=pub3)
 
 shop1 = Shop(name="Читай-Город")
 stock1 = Stock(book=book1, shop=shop1, count=24)
-sale1 = Sale(price=550, stock=stock1, count=43, date_sale=datetime.datetime(2023,10,20))
+sale1 = Sale(price=550, date_sale=datetime.datetime(2023, 10, 20), stock=stock1, count=43)
 stock2 = Stock(book=book2, shop=shop1, count=15)
-sale2 = Sale(price=645, stock=stock2, count=21, date_sale=datetime.datetime(2023,10,22))
+sale2 = Sale(price=645, date_sale=datetime.datetime(2023, 10, 22), stock=stock2, count=21)
 
 shop2 = Shop(name="Буквоед")
 stock3 = Stock(book=book2, shop=shop2, count=48)
-sale3 = Sale(price=335, stock=stock3, count=35, date_sale=datetime.datetime(2023,10,25))
+sale3 = Sale(price=335, date_sale=datetime.datetime(2023, 10, 25), stock=stock3, count=35)
 stock4 = Stock(book=book3, shop=shop2, count=56)
-sale4 = Sale(price=555, stock=stock4, count=12, date_sale=datetime.datetime(2023,10,27))
-
-
-Session = sessionmaker(bind=engine)
-session = Session()
+sale4 = Sale(price=555, date_sale=datetime.datetime(2023, 10, 27), stock=stock4, count=12)
 
 session.add_all([pub1, pub2, pub3, shop1, shop2, stock1, stock2, stock3, stock4, sale1, sale2, sale3, sale4])
 session.commit()
 
-pubs = input()
 
-res_inp = session.query(Publisher).filter(sq.or_(Publisher.id==pubs, Publisher.name==pubs)).all()[0]
-sales = session.query(Sale).join(Stock).join(Shop).join(Book).filter(Book.publisher==res_inp).subquery('t')
-shops = session.query(Shop).join(Stock).join(Sale).filter(Sale.id==sales.c.id)
-for s in shops:
-    print(s)
-
-session.close()
+if __name__ == '__main__':
+    pubs = input("Please enter a publisher name/id: ")
+    get_shops(session, pubs)
+    session.close()
--- models.py
+++ models.py
@@ -1,38 +1,34 @@
 import sqlalchemy as sq
+from sqlalchemy import MetaData, Column, Integer, String, Date
 from sqlalchemy.orm import declarative_base, relationship
 
-Base = declarative_base
+Base = declarative_base()
+metadata = MetaData()
 
 class Publisher(Base):
     __tablename__ = 'publishers'
-
     id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String(length=40), unique=True)
-
-    def __str__(self):
-        return f"Publisher {self.id}: {self.name}"
+    name = sq.Column(sq.String, unique=True)
+    def __repr__(self):
+        return f"Publisher (id={self.id}, name={self.name})"
 
 class Book(Base):
     __tablename__ = 'books'
-
     id = sq.Column(sq.Integer, primary_key=True)
-    title = sq.Column(sq.String(length=40), unique=True)
+    title = sq.Column(sq.String)
     publisher_id = sq.Column(sq.Integer, sq.ForeignKey('publishers.id'), nullable=False)
 
     publisher = relationship("Publisher", backref="books")
 
 class Shop(Base):
     __tablename__ = 'shops'
-
     id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String(length=40), unique=True)
-
-    def __str__(self):
-        return f"Shop {self.id}: {self.name}"
+    name = sq.Column(sq.String, unique=True)
+    def __repr__(self):
+        return f"Shop (id={self.id}, name={self.name})"
 
 class Stock(Base):
     __tablename__ = 'stocks'
-
     id = sq.Column(sq.Integer, primary_key=True)
     book_id = sq.Column(sq.Integer, sq.ForeignKey('books.id'), nullable=False)
     shop_id = sq.Column(sq.Integer, sq.ForeignKey('shops.id'), nullable=False)
@@ -44,16 +40,25 @@ class Stock(Base):
 
 class Sale(Base):
     __tablename__ = 'sales'
-
     id = sq.Column(sq.Integer, primary_key=True)
     price = sq.Column(sq.Float, nullable=False)
     date_sale = sq.Column(sq.Date)
     stock_id = sq.Column(sq.Integer, sq.ForeignKey('stocks.id'), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
 
-    # stock = relationship("Stock", backref="sales")
-
-
 def create_tables(engine):
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
+
+def get_shops(db_session, pubs):
+    result = db_session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).select_from(Shop). \
+        join(Stock). \
+        join(Book). \
+        join(Publisher). \
+        join(Sale)
+    if pubs.isdigit():
+        query = result.filter(Publisher.id == pubs).all()
+    else:
+        query = result.filter(Publisher.name == pubs).all()
+    for Book.title, Shop.name, Sale.price, Sale.date_sale in query:
+        print(f"{Book.title: <40} | {Shop.name: <10} | {Sale.price: <8} | {Sale.date_sale.strftime('%d-%m-%Y')}")
